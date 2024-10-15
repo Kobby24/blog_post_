717a42bd9d5f29dbeba1b03d0ab65e3de825b438
Kobby24 <geffah005@st.ug.edu.gh>
2024-10-15 23:26:00 +0000

    adding_port

--- main.py
+++ main.py
@@ -231,4 +231,5 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Fallback to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
